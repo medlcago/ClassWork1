90ff057aceb2abb93c9f9922c600b19d74cdf06b
medlcago <sasha.vol40@mail.ru>
2024-03-31 13:11:52 +0300

    fix

--- src/main/java/thread/ThreadExample.java
+++ src/main/java/thread/ThreadExample.java
@@ -6,16 +6,4 @@ public class ThreadExample implements Runnable {
         long threadId = Thread.currentThread().getId();
         System.out.printf("ID потока: %d\n", threadId);
     }
-
-    public static void main(String[] args) {
-        ThreadExample object = new ThreadExample();
-
-        Thread thread1 = new Thread(object);
-        Thread thread2 = new Thread(object);
-        Thread thread3 = new Thread(object);
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-    }
 }
